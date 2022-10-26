6c2921a6b8236dfd928eb64e9cec4c14f511a901
Amanda Richards <amandarichards@soliriusadmins-MacBook-Pro-3.local>
2022-10-26 16:03:35 +0100

    npm run build executed

--- growthhub/src/Components/SearchBar/SearchBar.js
+++ growthhub/src/Components/SearchBar/SearchBar.js
@@ -1,13 +1,38 @@
 import React from 'react'
 import css from './SearchBar.module.css';
+import {useState, useEffect} from 'react';
 
 const SearchBar = () => {
+
+    const [search, setSearch] = useState("");
+    const [results, setResults] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+          const response = await fetch(
+            `api/Subject/GetByName`
+          );
+          const data = await response.json();
+          setResults(data.data);
+        }
+        fetchData();
+      }, [search]);
+
+      function handleClick(){
+        setSearch('input')
+      }
+
   return (
     <div class={css.search}>
     <input placeholder="Search" class={css.searchBar} />
-    <button class={css.searchButton}>Search</button>
+    <button class={css.searchButton} onClick={handleClick}>Search</button>
 </div>
   )
 }
 
-export default SearchBar
+export default SearchBar;
+
+
+//set up a get by name route in the database 
+//onclick set the search term to be the input field value
+//make an api call when the search term changes
